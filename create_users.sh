f11098c7274bbfcffbeba47b6f972825679a9f19
Efosa Oviawe <64011917+Oviawe007@users.noreply.github.com>
2024-07-02 11:48:34 +0100

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -17,6 +17,24 @@
 
 
 #!/bin/bash
+# Author : Efosa Oviawe
+# Description: This Bash script automates user creation, group management, and password generation for new employees based on a user list file
+# Date: 30/06/2024
+
+#############################################################
+
+########### HOW TO USE ###########################
+
+
+# 1. Create a text file (e.g., user_list.txt) containing user information in the format username;groups (one user per line).
+# 2. Make the script executable: chmod +x create_users.sh
+# 3. Run the script: sudo ./create_users.sh
+
+
+###########################################################
+
+
+#!/bin/bash
 
 # Ensure script is run with root privileges
 if [[ $EUID -ne 0 ]]; then
@@ -57,19 +75,15 @@ create_user() {
     if ! grep -q "^$group:" /etc/group; then
       groupadd "$group" &>> "$log_file"
     fi
-    usermod -a -G "$group" "$username" &>> "$log_file"
-  done
-
-  # Generate random password (using command substitution)
- # password=$( (</dev/urandom tr -dc A-Za-z0-9!@#$%^&*() | head -c16) )
-
-                      
-    password=$(<<EOF
-    </dev/urandom
-    tr -dc A-Za-z0-9!@#$%^&*()
-    head -c16
-    EOF
-    )
+    usermod -a -G "$group" "$username" &>> "<span class="math-inline">log\_file"
+done
+\# Generate random password \(using here\-document\)
+password\=</span>(<<EOF
+</dev/urandom
+tr -dc A-Za-z0-9!@#$%^&*()
+head -c16
+EOF
+)
 
   echo "$username:$password" >> "$password_file"
   chmod 600 "$password_file" &>> "$log_file"
@@ -82,12 +96,8 @@ create_user() {
 
 # Function to log messages with timestamps
 log_message() {
-  message="$1"
-  echo "$(date +'%Y-%m-%d %H:%M:%S') - $message" >> "$log_file"
+  message="<span class="math-inline">1"
+echo "</span>(date +'%Y-%m-%d %H:%M:%S') - $message" >> "$log_file"
 }
 
-# Check if user list file exists
-if [ ! -f "$user_file" ]; then
-  echo "User list file '$user_file' not found. Please check the path." >&2
-  exit 1
-fi
+# Check if user list
